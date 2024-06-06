55b12e421dc4deea3ca6cd3b0d3f70a8b57bba12
Nithesh Naredla <Nithesh.Naredla@coxautoinc.com>
2024-06-06 18:01:53 +0530

    add the controller

---
+++ core-api-aws/Controllers/DynamoTableAccessController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace core_api_aws.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DynamoTableAccessController : ControllerBase
+    {
+
+        public DynamoTableAccessController() { }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAllRecords()
+        {
+            return Ok();
+
+        }
+        [HttpGet("GetBatch")]
+        public async Task GetBatch()
+        {
+
+        }
+
+        [HttpGet("GetItem")]
+        public async Task GetItem()
+        {
+
+        }
+
+        [HttpPost("AddItem")]
+        public async void AddItem()
+        {
+
+        }
+        [HttpDelete("DeleteItem")]
+        public async void DeleteItem()
+        {
+
+        }
+
+    }
+}
